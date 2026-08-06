106bd20dc00f4642e238740037fb95b5aaf00185
Barun <barunpandit321@gmail.com>
2026-08-06 19:09:17 +0530

    Remove email address and link Privacy Policy section 11 to Contact Us page

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,8 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, "index.html"),
                 steam: resolve(__dirname, "steam-calculator.html"),
-                privacy: resolve(__dirname, "privacy-policy.html")
+                privacy: resolve(__dirname, "privacy-policy.html"),
+                contact: resolve(__dirname, "contact.html")
             }
         }
     }
